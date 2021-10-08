cf22dba95e78ea8eff13501520c50300525ef109
syuusekizou <croissant.craft.boss@gmail.com>
2021-10-08 22:05:44 +0900

    新規作成

---
+++ SendComment/SendComment/SendComment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TwicasAPI.v2.api;
+
+namespace SendComment
+{
+    class SendComment
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var comment = new CommentAPI(GetConfigPath());
+                if (args.Length > 0)
+                {
+                    if (int.TryParse(args[0], out int value))
+                    {
+                        comment.MaxIndex = value;
+                    }
+                }
+                comment.PostComment();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 設定ファイルのパスを取得
+        /// </summary>
+        /// <returns>ファイルパス</returns>
+        static private string GetConfigPath()
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            return $"{path}config.json";
+        }
+    }
+}
